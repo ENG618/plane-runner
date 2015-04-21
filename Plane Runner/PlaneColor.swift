b217ddd64b6ad62b05c1d2d478dbd6eb12932357
eng618 <eng618@fullsail.edu>
2015-04-20 23:52:05 -0400

    Created PlaneColor strut to return string names for the different colors of the planes.

---
+++ Plane Runner/PlaneColor.swift
@@ -0,0 +1,28 @@
+//
+//  PlaneColor.swift
+//  Plane Runner
+//
+//  Created by Eric Garcia on 4/20/15.
+//  Copyright (c) 2015 Garcia Enterprise. All rights reserved.
+//
+
+import SpriteKit
+
+struct PlaneColor {
+    
+    func red () -> [String] {
+        return ["planeRed1", "planeRed2", "planeRed3"]
+    }
+    
+    func blue () -> [String] {
+        return ["planeBlue1", "planeBlue2", "planeBlue3"]
+    }
+    
+    func green () -> [String] {
+        return ["planeGreen1", "planeGreen2", "planeGreen3"]
+    }
+    
+    func yellow () -> [String] {
+        return ["planeYellow1", "planeYellow2", "planeYellow3"]
+    }
+}
