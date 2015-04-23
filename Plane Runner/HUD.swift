ba8989477656ba56f2218d1d19670442e3a72901
eng618 <eng618@fullsail.edu>
2015-04-23 01:43:06 -0400

    Created base method stubs for HUD class.

--- Plane Runner/HUD.swift
+++ Plane Runner/HUD.swift
@@ -13,16 +13,46 @@ class HUD {
     var distanceTraveled: Int
     var starsCollected: Int
     let distanceLabel: SKNode
+    let starsLabel: SKNode
     
     init() {
+        // Set initial values
         distanceTraveled = 0
         starsCollected = 0
         distanceLabel = SKNode()
-        setUpDistanceLable(distanceLabel)
+        starsLabel = SKNode()
         
+        
+        setUpDistanceLable(distanceLabel)
     }
     
     private func setUpDistanceLable(parent: SKNode) {
         // TODO: Set up label node
+    }
+    
+    func increaseDistance() {
+        // Adds yard to distance traveled
+        distanceTraveled++
+        // Update label
+        updateDistanceLabel()
+    }
+    
+    private func updateDistanceLabel() {
+        // TODO: Update distance label
+    }
+    
+    private func setUpStarsLabel(parent: SKNode) {
+        // TODO: Set up stars label
+    }
+    
+    func increaseStar() {
+        if starsCollected < 3 {
+            starsCollected++
+            updateStarsLabel()
+        }
+    }
+    
+    private func updateStarsLabel() {
+        // TODO: Update stars label
     }
 }
