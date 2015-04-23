025a17de3ade97ae19d75014875a4abe721c6c96
eng618 <eng618@fullsail.edu>
2015-04-23 01:49:18 -0400

    Created classes for levels 2 and 3.

---
+++ Plane Runner/LevelTwoScene.swift
@@ -0,0 +1,16 @@
+//
+//  LevelTwoScene.swift
+//  Plane Runner
+//
+//  Created by Eric Garcia on 4/23/15.
+//  Copyright (c) 2015 Garcia Enterprise. All rights reserved.
+//
+
+import SpriteKit
+
+class LevelTwoScene: SKScene {
+    
+    override func didMoveToView(view: SKView) {
+        // Setup scene here
+    }
+}
